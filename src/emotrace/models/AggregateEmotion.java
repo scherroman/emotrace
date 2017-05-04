5ea0bf0760ef92410514f15edde3cc495e75efb9
Edgar Samudio <edgar.samudiob@gmail.com>
2017-05-03 22:47:57 -0400

    recreated AggregateEmotion file

---
+++ src/emotrace/models/AggregateEmotion.java
@@ -0,0 +1,16 @@
+package emotrace.models;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+/**
+ * Created by esamudio on 5/3/17.
+ */
+@Entity
+public class AggregateEmotion {
+
+
+
+}
